0a066b0bd82b250615a5b3cb790f8636af3939ef
ritesh-shar <riteshksharma2006@gmail.com>
2026-02-15 20:17:58 +0530

    Solved race condition issue

--- server/controllers/rentalController.js
+++ server/controllers/rentalController.js
@@ -2,81 +2,96 @@ const rental = require('../models/rental.js');
 const tool = require('../models/tool.js');
 
 const startRental = async (req,res) => {
+    const session = await rental.startSession();
+    session.startTransaction();
     try{
     const toolId = req.params.id;
     const renterId = req.user.id;
     
-    const toolToRent = await tool.findById(toolId);
+    const toolToRent = await tool.findById(toolId).session(session);
+
     if(!toolToRent){
-        return res.status(404).json({message:"Tool not found"});
+        throw {status: 404 ,message:"Tool not found"};
     }
     if(toolToRent.owner.toString() === renterId){
-        return res.status(403).json({message:"Cannot rent what you own"});
+        throw {status : 403 , message:"Cannot rent what you own"};
     }
 
     if(!toolToRent.isAvailable){
-        return res.status(400).json({message:"Tool already in use"});
+        throw {status: 400, message:"Tool already in use"};
     }
 
-    const newRental = await rental.create({
+    toolToRent.isAvailable = false;
+    toolToRent.renter =  renterId;
+    await toolToRent.save({session: session});
+
+    const newRental = await rental.create([{
         renter: renterId,
         owner: toolToRent.owner,
         tool: toolToRent._id,
         pricePerHour: toolToRent.pricePerHour,
         status:'Active'
-    })
+     }], {session: session});
 
-    toolToRent.isAvailable = false;
-    toolToRent.renter = renterId;
-    await toolToRent.save();
+
+     await session.commitTransaction();
 
     return res.status(201).json({
-        rentalid: newRental.id,
-        toolId: toolToRent.id,
-        starttime: newRental.rentTimeStart,
+        rentalid: newRental[0]._id,
+        toolId: toolToRent._id,
+        starttime: newRental[0].rentTimeStart,
     })
 
     }
 
     catch(error){
-        return res.status(500).json({message:"Error:",error:error.message});
+        await session.abortTransaction();
+        return res.status(error.status || 500).json({message:"Error:",error:error.message});
+    }
+
+    finally{
+        await session.endSession();
     }
 
 }
 
 const endRental = async (req,res) =>{
+    const session = await rental.startSession();
+    session.startTransaction();
 
     try{
         const rentalid = req.params.id;
-        const myRental = await rental.findById(rentalid)
+        const myRental = await rental.findById(rentalid).session(session);
         if(!myRental){
-            return res.status(404).json({message:"Rental not found"});
+            throw {status: 404, message:"Rental not found"};
         }
         if(myRental.status != 'Active'){
-            return res.status(400).json({message:"Tool not in use"});
+            throw {status: 400, message:"Tool not in use"};
         }
         if(myRental.renter.toString() != req.user.id){
-            return res.status(403).json({message:"Not authorised to end the rental"});
+            throw {status: 403, message:"Not authorised to end the rental"};
         }
 
         const findTool = myRental.tool;
-        const myTool = await tool.findById(findTool);
+        const myTool = await tool.findById(findTool).session(session);
         if(!myTool || !myTool.renter || myTool.renter.toString() != req.user.id){
-            return res.status(400).json({message:"Not renting tool"});
+            throw {status: 400, message:"Not renting tool"};
         }
 
         myRental.status = 'Completed';
         myRental.rentTimeEnd = Date.now();
 
         const durationMS = myRental.rentTimeEnd - myRental.rentTimeStart;
-        const hours = Math.ceil(durationMS/(1000*60*60));
+        const hours = Math.max(1,Math.ceil((durationMS/(1000*60*60))));
         myRental.totalCost = myRental.pricePerHour*hours;
 
         myTool.isAvailable = true;
         myTool.renter = null;
     
-        await myTool.save();
-        await myRental.save();
+        await myTool.save({session: session});
+        await myRental.save({session: session});
+
+        await session.commitTransaction();
 
         return res.status(200).json({
             message:"Rental Ended",
@@ -85,7 +100,12 @@ const endRental = async (req,res) =>{
 
     }
     catch(error){
-        return res.status(500).json({message:"Error:",error:error.message});
+        await session.abortTransaction();
+        return res.status(error.status || 500).json({message:"Error:",error:error.message});
+    }
+
+    finally{
+        await session.endSession();
     }
 
 }
