6fcbb069ff09c07e14178dec4e7e3258b98ba6a0
ritesh-shar <riteshksharma2006@gmail.com>
2026-03-15 13:51:07 +0530

    Remove Logging of errors in console

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const {z} = require('zod');
 const errorHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
-    let message = err.message;
+    let message = err.message;  
     if(err.name === 'ZodError' || err.errors){
         statusCode = 400;
         message = err.errors?.map(e => e.message).join(", ");
